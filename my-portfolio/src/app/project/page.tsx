827f5f2088d061fb2ed95bd4e1b9083680f07860
Hiroki Inoue <tyynh.hiroki@gmail.com>
2025-10-26 22:29:47 -0700

    created the template of project page

--- my-portfolio/src/app/project/page.tsx
+++ my-portfolio/src/app/project/page.tsx
@@ -1,8 +1,26 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
+import styles from './page.module.scss'
+import MyJobProjectList from '../component/MyJobProjectList'
+import MyOwnProjectList from '../component/MyOwnProjectList'
 
 const MyProject = () => {
+  const [page, setPage] = useState<"job" | "own">("job")
+
   return (
-    <div>page</div>
+    <div className={styles.project}>
+      <h1 className={styles.h1}>My Project</h1>
+      <nav className={styles.nav}>
+        <button type='button' className={page === "job" ? `${styles.button} ${styles.selected}` : `${styles.button}`} onClick={() => setPage("job")}>My Job Project</button>
+        <button type='button' className={page === "own" ? `${styles.button} ${styles.selected}` : `${styles.button}`} onClick={() => setPage("own")}>My Own Project</button>
+      </nav>
+      {page === "job" ? (
+        <MyJobProjectList />
+      ) : (
+        <MyOwnProjectList />
+      )}
+    </div>
   )
 }
 
